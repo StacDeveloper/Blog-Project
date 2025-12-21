4a5e2323502644bbf1134754101c926e2d2bcb15
Soham's GitHub <sohammule09@gmail.com>
2025-12-21 13:39:09 +0000

    Delete frontend/frontend/.next directory

--- frontend/frontend/.next/dev/static/chunks/3b189_jodit-react_build_jodit-react_8fc1a51c.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/3b189_jodit-react_build_jodit-react_66016968.js",
-  "static/chunks/3b189_jodit-react_build_jodit-react_0310984e.js"
-],
-    source: "dynamic"
-});
--- frontend/frontend/.next/dev/static/chunks/frontend_frontend_src_app_layout_tsx_4f7a6fc4._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__5ea78e44._.css",
-  "static/chunks/3b189_8f53c944._.js",
-  "static/chunks/frontend_frontend_src_7e81f3e0._.js"
-],
-    source: "dynamic"
-});
