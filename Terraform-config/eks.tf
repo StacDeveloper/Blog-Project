b511bb71c9ea29753f32fb339cfa9b49d11432f1
Soham's GitHub <sohammule09@gmail.com>
2025-12-27 05:18:55 +0000

    Add files via upload

---
+++ Terraform-config/eks.tf
@@ -0,0 +1,14 @@
+resource "aws_eks_cluster" "eks_cluster" {
+  name = "Blog-project"
+
+  role_arn = aws_iam_role.cluster_role.arn
+
+  vpc_config {
+    subnet_ids = data.aws_subnets.public.ids
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.cluster_AmazonEKSClusterPolicy,
+  ]
+
+}
