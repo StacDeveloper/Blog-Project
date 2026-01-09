856c243c5e3459cfb98e816f62c2318cbeade627
Soham Mule <sohammule09@gmail.com>
2026-01-09 23:53:59 +0000

    testing cicd

--- services/user-service/src/server.ts
+++ services/user-service/src/server.ts
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV !== "test") {
     await connectDB()
     await cloudConfiguration()
 }
-// aaaaaaaaaaaaaaaaaaaaaasdasdasda
+// aaaaaaaaaaaaaaaaaaaaaasdasdasd
 app.use("/api/user", userRouter)
 app.use("/try", (req, res) => {
     res.json({ success: true, message: "Trying CI/CD" })
